2fdf7ee9e21842aed4fa83e169eae0abd25df7bb
Tandu Yanmick Ateh <95031570+G-GraceCode@users.noreply.github.com>
2023-12-10 13:44:24 +0000

    Update 2 files

--- frontend/src/components/Layout/Dashboard.jsx
+++ frontend/src/components/Layout/Dashboard.jsx
@@ -9,17 +9,7 @@ const Dashboard = ({ bar }) => {
   return (
     <div className="content">
       <Navbar bar={bar} />
-      <HomeSlide>
-        <div className="Userinfo">
-          <div>
-            <h1>{`Welcome Back ${userInfo.username}`}</h1>
-            <p>What New do you love adding today in Projectject</p>
-            <Link to="/project" className="proj">
-              Create a New Project
-            </Link>
-          </div>
-        </div>
-      </HomeSlide>
+      <HomeSlide></HomeSlide>
     </div>
   );
 };
@@ -29,29 +19,7 @@ export default Dashboard;
 const HomeSlide = styled.div`
   position: relative;
   padding: 0 1rem;
-  .Userinfo {
-    position: absolute;
-    bottom: 4%;
-    top: 4%;
-    left: 7%;
-    right: 7%;
-    padding: 0 1rem;
-    z-index: 100;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    & > div {
-      text-align: center;
-      border-radius: var(--border-radius);
-      padding: 1.5rem 2rem;
-      background-color: var(--natural-white);
-      filter: drop-shadow(0 2px 5px rgba(0, 0, 0, 0.35));
-      h1 {
-        margin-bottom: 1rem;
-      }
-      .proj {
-        text-decoration: none;
-      }
-    }
-  }
+  width: 80%;
+  margin: 0 auto;
+  padding: 0.4rem 0.6rem;
 `;
