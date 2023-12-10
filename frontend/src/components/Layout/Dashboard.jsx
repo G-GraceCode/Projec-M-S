85d5c707415e8434e601aae3803ca12a8cadd5b8
Tandu Yanmick Ateh <95031570+G-GraceCode@users.noreply.github.com>
2023-12-10 14:38:32 +0000

    Update Dashboard.jsx

--- frontend/src/components/Layout/Dashboard.jsx
+++ frontend/src/components/Layout/Dashboard.jsx
@@ -4,20 +4,21 @@ import { Link } from "react-router-dom";
 import { userAuth } from "../../ultContext/AuthContext";
 import { Form, Button, Row, Col, Card } from "react-bootstrap";
 import { RiArrowRightSLine } from "react-icons/ri";
+import poc from "../../assets/poc.jpg";
 
-const Dashboard = ({ bar }) => {
+const Dashboard = () => {
   const { userInfo } = userAuth();
 
   return (
     <div className="content">
-      <Navbar bar={bar} />
+      <Navbar />
       <HomeSlide>
         <Recentpost>
           <h4 className="my-3 text-left text-[18px] text-[var(--natural-white)]">
             Top Recent Pojects
           </h4>
           <div className="cards">
-            <div>
+            <div className="card">
               <Col className="p-3">
                 <h2>
                   How to create public shareable link for private user resources
@@ -29,12 +30,12 @@ const Dashboard = ({ bar }) => {
                   how we give access to files on google drive without the person
                   requiring to login
                 </h6>
-                <button>
+                <button className="px-1 py-3">
                   Read More <RiArrowRightSLine />
                 </button>
               </Col>
-              <div>
-                <img />
+              <div className="image">
+                <img src={poc} />
               </div>
             </div>
           </div>
@@ -54,4 +55,23 @@ const HomeSlide = styled.div`
   padding: 0.4rem 0.6rem;
 `;
 
-const Recentpost = styled.div``;
+const Recentpost = styled.div`
+  .cards {
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(min(450px), 1fr));
+    align-items: center;
+    justify-content: center;
+
+    .card {
+      display: flex;
+      align-items: center;
+
+      .image {
+        img {
+          width: 100%;
+          object-fit: cover;
+        }
+      }
+    }
+  }
+`;
