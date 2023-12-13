f077940d6bb4c4efab9badfa5c65d3ad6ee6658d
Tandu Yanmick Ateh <95031570+G-GraceCode@users.noreply.github.com>
2023-12-13 06:53:09 +0000

    Update 7 files

---
+++ backend/cloudinaryUpload/cloudUpload.js
@@ -0,0 +1,23 @@
+import dotenv from "dotenv";
+dotenv.config();
+import { v2 as cloudinary } from "cloudinary";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET,
+});
+
+const handleUpload = async (file) => {
+  try {
+    const imgRes = await cloudinary.uploader.upload(file, {
+      resource_type: "auto",
+    });
+
+    return imgRes;
+  } catch (e) {
+    console.log(e.message);
+  }
+};
+
+export default handleUpload;
