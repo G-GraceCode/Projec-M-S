9c3917ba53fa9af62d6ea4624bb336bfc6245924
Tandu Yanmick Ateh <95031570+G-GraceCode@users.noreply.github.com>
2023-12-31 13:52:46 +0000

    Update AuthContext.jsx

--- frontend/src/ultContext/AuthContext.jsx
+++ frontend/src/ultContext/AuthContext.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { useSnackbar } from "notistack";
 
-const AuthContext = createContext({});
+const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
   const [userInfo, setUserInfo] = useState({});
