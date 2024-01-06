adaefdd1122c2a51b2ebd52753b0eb74f3db657b
Tandu Yanmick Ateh <95031570+G-GraceCode@users.noreply.github.com>
2024-01-06 14:20:27 +0000

    Update Search.jsx and index.css

--- frontend/src/components/Search.jsx
+++ frontend/src/components/Search.jsx
@@ -163,12 +163,12 @@ const Searchproject = styled.div`
       outline: 2px solid transparent;
       border-radius: var(--border-radius-2);
     }
-    input[type="text"]:focus {{
+    input[type="text"]:focus {
       outline: 2px solid var(--color-green);
       outline-offset: 3px;
       width: 430px;
       transition: outline var(--transition);
-      transition: width var(--transition);}
+      transition: width var(--transition);
     }
   }
 `;
